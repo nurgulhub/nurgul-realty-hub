38d1b7eec528e714f90fabcb42abd6c9c98d47dd
nurgulhub <frapeba@icloud.com>
2026-01-19 15:46:54 +0600

    Update properties-data.js
    ];
     219

--- properties-data.js
+++ properties-data.js
@@ -216,11 +216,7 @@ walking distance to Panfilov Park, Dubovy Park, restaurants, shops, banks, clini
     { name: "Нургуль", phone: "0559500551" },
     { name: "Нагима",  phone: "0706315903" }
   ]
-},
-
-
-
-
+];
 
 /* ============================================================
    🔜 HOUSE 2–7
